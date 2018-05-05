25604e58e71205169db1beea04a12de92310e352
sleshJdev <slesh.eugene93@gmail.com>
2018-05-05 11:57:54 +0300

    renamed a web config

--- src/main/java/com/slesh/gallery/config/AppInitializer.java
+++ src/main/java/com/slesh/gallery/config/AppInitializer.java
@@ -15,7 +15,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{WebConfiguration.class};
+        return new Class[]{WebConfig.class};
     }
 
     @Override
