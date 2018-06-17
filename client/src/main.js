1ba5af384071e23988fdcb745b9de3b2299677bc
sleshJdev <slesh.eugene93@gmail.com>
2018-06-17 18:19:28 +0300

    implemented review by click on the image, button showing a phone number for authorized users only

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import BootstrapVue from 'bootstrap-vue';
+import { Modal } from 'bootstrap-vue/es/components';
+import { Modal as ModalDirective } from 'bootstrap-vue/es/directives';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import Vue from 'vue';
@@ -9,6 +11,8 @@ import router from './router';
 import Http from './components/utils/Http';
 
 Vue.use(BootstrapVue);
+Vue.use(ModalDirective);
+Vue.use(Modal);
 Vue.config.productionTip = false;
 
 Http.setRouter(router);
