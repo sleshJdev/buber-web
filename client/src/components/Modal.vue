5cf91d3fa30cf2b57736ec5a5724d90aef151707
sleshJdev <slesh.eugene93@gmail.com>
2018-07-28 16:52:13 +0300

    added missing modal component

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-modal id="unauthorizedModal" title="Need to sign in"
+           size="sm" :lazy="true"
+           ok-variant="success" cancel-variant="secondary"
+           cancel-title="Dismiss" ok-title="Sign in"
+           @ok="$router.push({name: 'sign-in'})">
+    <p class="text-center">
+      Please, sign in to review ads and see phone numbers
+    </p>
+  </b-modal>
+</template>
+
+<script>
+  export default {
+    name: 'modal',
+  };
+</script>
+
+<style scoped>
+
+</style>
