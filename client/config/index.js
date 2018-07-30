ee9dfd5d0c95916aef371b3ff0ee7dfb7d3ea956
sleshJdev <slesh.eugene93@gmail.com>
2018-07-30 23:37:18 +0300

    change client/server ports

--- client/config/index.js
+++ client/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       "/api": {
-        target: "http://localhost:7222",
+        target: "http://localhost:8080",
         secure: false,
         logLevel: "debug",
       }
@@ -20,7 +20,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 7333, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
